1da825b7f7e1cdfba8da76889dbb462195265367
EC2 Default User <ec2-user@ip-172-31-38-36.ap-northeast-1.compute.internal>
2021-08-07 11:38:21 +0000

    完了

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
    resource :favorites, only: [:create,:destroy]
    resources :book_comments, only: [:create, :destroy]
   end
- resources :users,only: [:show,:index,:edit,:update]
+ resources :users,only: [:show,:index,:edit,:update] do
+  resource :relationships, only: [:create, :destroy]
+  get 'followings' => 'relationships#followings', as: 'followings'
+  get 'followers' => 'relationships#followers', as: 'followers'
+ end
   
  
 end
